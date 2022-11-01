5b39efff0c7f3673c11aa44f40aa1255bd30c460
Bravineugenio <101228018+Bravineugenio@users.noreply.github.com>
2022-11-01 19:37:15 -0300

    feat: loginInstitutions

---
+++ src/services/institution/loginInstitution.service.ts
@@ -0,0 +1,35 @@
+import { IInstitutionLogin } from "../../interfaces/institutions";
+import AppDataSource from "../../data-source";
+import { Institution } from "../../entities/Institution.entity";
+import * as bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { AppError } from "../../errors/AppError";
+
+const institutionLoginService = async ({ email, password }: IInstitutionLogin) => {
+  const institutionRepository = AppDataSource.getRepository(Institution);
+
+  const intitutions = await institutionRepository.find();
+
+  const account = intitutions.find((institution) => institution.email === email);
+
+  if (!account) {
+    throw new AppError(401, "Account not found");
+  }
+
+  if (!bcryptjs.compareSync(password, account.password)) {
+    throw new AppError(401, "Wrong email/password");
+  }
+
+  const token = jwt.sign(
+    {
+      email: email,
+      id: account.id,
+    },
+    String(process.env.SECRET_KEY),
+    { expiresIn: "24h" }
+  );
+
+  return token;
+};
+
+export default institutionLoginService;
