8de07d4c9bddd75fe5e7272786febe75d5ad16f7
Bravineugenio <101228018+Bravineugenio@users.noreply.github.com>
2022-11-02 17:23:36 -0300

    feat: completing adminTests

--- src/routes/institutions.routes.ts
+++ src/routes/institutions.routes.ts
@@ -18,7 +18,6 @@ export const institutionsRoutes = () => {
     routes.post("/", createInstitutionController)
     routes.get("/:id",ensureInstitutionAuthMiddleware,ensureIsInstitutionMiddleware, getMyInstitutionsController)
     routes.post("/login", institutionLoginController)
-    routes.delete("/:id",ensureInstitutionAuthMiddleware, deleteInstitutionController)
-    routes.patch("/:id",ensureInstitutionAuthMiddleware, updateInstitutionController)
+
     return routes
 }
