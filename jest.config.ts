d61efe5297d91c31df87a6a3333e9a2f01e81b0a
Bravineugenio <101228018+Bravineugenio@users.noreply.github.com>
2022-11-02 13:58:16 -0300

    test

--- jest.config.ts
+++ jest.config.ts
@@ -154,7 +154,7 @@ export default {
 
   // The glob patterns Jest uses to detect test files
    testMatch: [
-    "**/**/*.spec.ts",
+    "**/**/*.spec.ts"
    ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
