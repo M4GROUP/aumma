3af4a53712bda1b13dda78efb5362e961ee93ac6
jefersonokdemais <jefersonokdemais@hotmail.com>
2022-11-07 13:14:54 -0300

    fix: routes Schedules add two modified

--- src/routes/schedules/schedules.routes.ts
+++ src/routes/schedules/schedules.routes.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 
 import createScheduleController from "../../controllers/schedules/createSchedule.controller";
 import { deleteSchedules } from "../../controllers/schedules/deleteSchedules";
+import { listScheduleByInstitution } from "../../controllers/schedules/listScheduleByInstitution";
 import { listSchedules } from "../../controllers/schedules/listSchedules";
+import { listSchedulesByChildren } from "../../controllers/schedules/listSchedulesByChildren";
 import ensureInstitutionAuthMiddleware from "../../middlewares/institutions/ensureAuthInstitution.middleware";
 
 const routes = Router()
@@ -12,8 +14,8 @@ const schedulesRoutes = () => {
     routes.post("/:id", ensureInstitutionAuthMiddleware, createScheduleController)
     routes.get("", listSchedules)
     routes.delete("/:id", deleteSchedules)
-    routes.get("/:id/mother", /* listAllMotherSchedules */)
-    routes.get("/:id/intitution", /* listAllIntituitionSchedules */)
+    routes.get("/:id/children", listSchedulesByChildren)
+    routes.get("/:id/intitution", listScheduleByInstitution)
 
     return routes
 
