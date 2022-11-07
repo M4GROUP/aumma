f42e3f8a9b393a9d539b3b2f4f3e14e893cd57cd
jefersonokdemais <jefersonokdemais@hotmail.com>
2022-11-07 11:28:28 -0300

    feat: equalize group arquives

--- src/routes/schedules/schedules.routes.ts
+++ src/routes/schedules/schedules.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 
 import createScheduleController from "../../controllers/schedules/createSchedule.controller";
+import { deleteSchedules } from "../../controllers/schedules/deleteSchedules";
+import { listSchedules } from "../../controllers/schedules/listSchedules";
 import ensureInstitutionAuthMiddleware from "../../middlewares/institutions/ensureAuthInstitution.middleware";
 
 const routes = Router()
@@ -8,8 +10,8 @@ const routes = Router()
 const schedulesRoutes = () => {
 
     routes.post("/:id", ensureInstitutionAuthMiddleware, createScheduleController)
-    routes.get("", /* listAllSchedules PRONTA*/)
-    routes.delete("/:id", /* SOFTdeleteSchedule PRONTA/)
+    routes.get("", listSchedules)
+    routes.delete("/:id", deleteSchedules)
     routes.get("/:id/mother", /* listAllMotherSchedules */)
     routes.get("/:id/intitution", /* listAllIntituitionSchedules */)
 
